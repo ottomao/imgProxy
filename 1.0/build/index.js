89568bb0c3ad19f6487c8e9a985ab0e2ebf52e67
Jiali <xiaofeng.mxf@taobao.com>
2013-12-25 14:49:04 +0800

    update files and guide

--- 1.0/build/index.js
+++ 1.0/build/index.js
@@ -8,27 +8,115 @@ gallery/imgProxy/1.0/index
  * @fileoverview 
  * @author 加里 茅晓锋<xiaofeng.mxf@taobao.com>
  * @module imgProxy
+ * mod from https://github.com/thatguystone/imgproxy
  **/
 KISSY.add('gallery/imgProxy/1.0/index',function (S, Node,Base) {
-    var EMPTY = '';
-    var $ = Node.all;
-    /**
-     * 
-     * @class ImgProxy
-     * @constructor
-     * @extends Base
-     */
-    function ImgProxy(comConfig) {
-        var self = this;
-        //调用父类构造函数
-        ImgProxy.superclass.constructor.call(self, comConfig);
+
+    var SWF_FILE =  "http://a.tbcdn.cn/s/kissy/gallery/drawingPad/2.0/ImgProxy.swf";
+    var objWrapper, //the swf used for remote loading
+        loading = false,
+        ready   = false,
+        id = 0,
+        waiting = [],
+        callbacks = {},
+        flashObj;
+    
+    function createSwf(idOrEl, vars) {
+
+        var swfTpl ='<object data="__swfUrl" type="application/x-shockwave-flash" id="imgproxy" width="100%" height="100%">'+
+                        '<param name="allowScriptAccess" value="always">'+
+                        '<param name="flashvars" value="">'+
+                        '<param name="wmode" value="transparent">'+
+                        '<param name="movie" value="__swfUrl">'+
+                    '</object>';
+
+        var swfNode = Node(swfTpl.replace(/__swfUrl/g,SWF_FILE));
+
+        idOrEl = Node.one('#' + idOrEl);
+        idOrEl.append(swfNode);
+        flashObj = swfNode.getDOMNode();
     }
-    S.extend(ImgProxy, Base, /** @lends ImgProxy.prototype*/{
+    
+    function loadSwf() {
+        if (loading) {
+            return;
+        }
+        loading = true;
+        
+        objWrapper = Node('<div></div>')
+            .attr('id', 'imgproxy')
+            .css({
+                height: 1,
+                position: 'absolute',
+                top: 0,
+                width: 1
+            });
+        
+        Node.one('body').append(objWrapper);
+        
+        createSwf('imgproxy');
+    }
+    
+    window.imgproxy = {
+        load: function(url, successCallback, failCallback) {
+            if (!ready) {
+                waiting.push(Array.prototype.slice.call(arguments));
+                loadSwf();
+                return;
+            }
+            
+            var localId = id++;
+            
+            this._callbacks(localId, true, successCallback, failCallback);
+            flashObj.load(localId,url);
+        },
+        
+        _callbacks: function(id, single, successCallback, failCallback) {
+            callbacks[id] = {
+                single: single,
+                success: successCallback,
+                error: failCallback
+            };
+        },
+        
+        _onLoad: function() {
+            var i;
+            
+            ready = true;
+            
+            if (waiting.length) {
+                for (i = 0; i < waiting.length; i++) {
+                    window.imgproxy.load.apply(this, waiting[i]);
+                }
+            }
+            
+            waiting = null;
+        },
+        _onSuccess: function(id, img) {
+            var fn = callbacks[id].success;
+            if (fn) {
+                fn('data:image/png;base64,' + img);
+            }
+            
+            if (callbacks[id].single) {
+                delete callbacks[id];
+            }
+        },
+        _onError: function(id, error) {
+            var fn = callbacks[id].error;
+            if (fn) {
+                fn(error);
+            }
+            
+            if (callbacks[id].single) {
+                delete callbacks[id];
+            }
+        }
+    };
 
-    }, {ATTRS : /** @lends ImgProxy*/{
+    return window.imgproxy; 
 
-    }});
-    return ImgProxy;
+
 }, {requires:['node', 'base']});
 
 
--- 1.0/guide/index.md
+++ 1.0/guide/index.md
@@ -1,15 +1,26 @@
 ## 综述
 
-ImgProxy是。
+ImgProxy是一个图片代理工具。它通过Flash引入跨域的图片，做png-base64编码，回传给页面。一般用于解决canvas上的跨域getImageData问题。
 
 * 版本：1.0
 * 作者：加里 茅晓锋
 * demo：[http://gallery.kissyui.com/imgProxy/1.0/demo/index.html](http://gallery.kissyui.com/imgProxy/1.0/demo/index.html)
 
-## 初始化组件
+## 组件使用
 
     S.use('gallery/imgProxy/1.0/index', function (S, ImgProxy) {
-         var imgProxy = new ImgProxy();
+    	var url = "http://img04.taobaocdn.com/bao/uploaded/i4/T1fQpaXtBXXXb1upjX.jpg";
+    	Imgproxy.load(url,
+    	    function(imgBase64) {
+    	        S.log(imgBase64);
+    	    },
+    	    function(msg) {
+    	        S.log("fail to load img: " + msg);
+    	    }
+    	);
     })
 
-## API说明
+## 注意事项
+
+* flash跨域引入图片时，需要有crossdomain.xml，且引用图片的站点位于其白名单中。
+* e.g. http://img04.taobaocdn.com/crossdomain.xml
